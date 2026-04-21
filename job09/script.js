3e3cba008189aa71274cb016a207831f7695ec5f
faOum <attouman.farouk@gmail.com>
2026-04-21 09:46:12 +0200

    feat : implementation de la fonction 'tri'

---
+++ job09/script.js
@@ -0,0 +1,22 @@
+function tri(numbers, order) {
+
+    for (let i = 0; i < numbers.length; i++) {
+        for (let j = i + 1; j < numbers.length; j++) {
+
+            if (
+                (order === "asc" && numbers[i] > numbers[j]) ||
+                (order === "desc" && numbers[i] < numbers[j])
+            ) {
+                // échange
+                let temp = numbers[i];
+                numbers[i] = numbers[j];
+                numbers[j] = temp;
+            }
+        }
+    }
+
+    return numbers;
+}
+
+console.log(tri([5, 2, 9, 1], "asc")); 
+console.log(tri([5, 2, 9, 1], "desc"));
