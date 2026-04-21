e02c43545b943141531a295b70b2f39643828473
faOum <attouman.farouk@gmail.com>
2026-04-21 09:52:22 +0200

    feat : implementation de la fonction 'compterVoyelles'

---
+++ job10/script.js
@@ -0,0 +1,18 @@
+function compterVoyelles(phrase) {
+
+    const voyelles = "aeiouy";
+    let compteur = 0;
+
+    for (let i = 0; i < phrase.length; i++) {
+        let lettre = phrase[i].toLowerCase();
+
+        if (voyelles.includes(lettre)) {
+            compteur++;
+        }
+    }
+
+    console.log(`La phrase contient ${compteur} voyelles`);
+}
+
+
+compterVoyelles("Bonjour tout le monde");
